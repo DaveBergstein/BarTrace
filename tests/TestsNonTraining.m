a6d93da387c080e2d78cdee09ae81f305b8a564b
dbergstein <dbergstein@tess-dbergstein-mbp.lan1>
2020-03-31 19:34:32 -0400

    added tests

--- tests/TestsNonTraining.m
+++ tests/TestsNonTraining.m
@@ -1,76 +1,54 @@
 %% Test Set
 
-
-%% Test2
-% Test that starting script runs
-disp("Test2")
-
+%% Test1
+disp("Test that starting script runs")
 StartHere
 close all
 
-%% Test3
-% Import video
-disp("Test3")
-
-vid = liftVid("\data\Data2020_02_18\2020_02_18_2_Trim.mp4");
+%% Test2
+disp("Import video")
+vid = liftVid("DaveShort.mp4");
 vid.loadVideo;
-assert(vid.vidLength == 189)
+assert(vid.vidLength == 62)
+close all
+
+%% Test3
+disp("Find plate with model A")
+vid = liftVid("RandyShort.mp4");
+plateRadius = 200;
+vid.tracePlate("modelA", plateRadius)
+% Check the position
+assert(vid.centers(25,1) > 325)
+assert(vid.centers(25,1) < 375)
+assert(vid.centers(25,2) > 650)
+assert(vid.centers(25,2) < 700)
 close all
 
 %% Test4
-% Find plate
-disp("Test4")
-
-vid = liftVid("\data\Data2020_02_18\2020_02_18_2_Trim.mp4");
-plateRadius = 77;
-vid.tracePlate("modelA", plateRadius)
+disp("Find plate with model C")
+vid = liftVid("RandyShort.mp4");
+vid.tracePlate("modelC")
 % Check the position
-assert(vid.centers(100,1) > 350)
-assert(vid.centers(100,1) < 400)
-assert(vid.centers(100,2) > 630)
-assert(vid.centers(100,2) < 720)
+assert(vid.centers(25,1) > 325)
+assert(vid.centers(25,1) < 375)
+assert(vid.centers(25,2) > 650)
+assert(vid.centers(25,2) < 700)
 close all
 
 %% Test5
-% Find plate
-disp("Test5")
-
-vid = liftVid("\data\Data2020_02_18\2020_02_18_2_Trim.mp4");
-vid.tracePlate("modelB")
-% Check the position
-assert(vid.centers(100,1) > 350)
-assert(vid.centers(100,1) < 400)
-assert(vid.centers(100,2) > 630)
-assert(vid.centers(100,2) < 720)
-close all
-
-%% Test6
-% Find plate
-disp("Test6")
-
-vid = liftVid("\data\Data2020_02_18\2020_02_18_2_Trim.mp4");
+disp("check that result is saved")
+delete("result1.mp4");
+vid = liftVid("RandyShort.mp4");
 vid.tracePlate("modelC")
-% Check the position
-assert(vid.centers(100,1) > 350)
-assert(vid.centers(100,1) < 400)
-assert(vid.centers(100,2) > 630)
-assert(vid.centers(100,2) < 720)
-close all
-
-%% Test7
-% Write results and video
-disp("Test7")
-
-vid = liftVid("\data\ForTesting\RandyShort.mp4");
-vid.tracePlate("modelB")
+vid.annotate;
 vid.save("result1")
 % Check that files were created
 assert(exist("result1.mp4", "file") == 2)
 close all
 
-%% Test 8
-% Try reading and processing frame-by-frame
-vid = liftVid("\data\ForTesting\DaveShort.mp4");
+%% Test6
+disp("Try reading and processing frame-by-frame");
+vid = liftVid("DaveShort.mp4");
 vid.nextFrame = 0;
 figure;
 while vid.nextFrame >= 0
